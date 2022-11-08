7f3ed75d30e6f196d2f2fdede37bce85b8ffc746
waterbinnn <ewaterbin@naver.com>
2022-11-08 15:51:02 +0900

    🚧 Implement Header

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Pressable } from 'react-native';
+import { theme } from './color';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+      <TouchableOpacity>
+        <Text style={styles.btnText}>Work</Text>
+      </TouchableOpacity>
+      <TouchableHighlight underlayColor={'yellow'} activeOpacity={0.5} onPress={()=>console.log('press')}>
+        <Text style={styles.btnText}>Travel</Text>
+      </TouchableHighlight>
+      </View>
     </View>
   );
 }
@@ -13,8 +21,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#000',
+    paddingHorizontal:20,
   },
+  header: {
+    flexDirection:'row',
+    justifyContent:'space-between',
+    marginTop:100,
+  },
+  btnText: {
+    fontSize:30,
+    fontWeight:'700',
+    // color:theme.grey,
+    color:'white',
+  }
 });
