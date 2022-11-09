d05f143a73b961a79c386c8b6441dcdb1c6dee8b
waterbinnn <ewaterbin@naver.com>
2022-11-09 20:06:37 +0900

    🚧 Implement TextInput

--- App.js
+++ App.js
@@ -1,19 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Pressable } from 'react-native';
+import { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback,
+  Pressable,
+  TextInput,
+} from 'react-native';
 import { theme } from './color';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const [text, setText] = useState('');
+
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+  const onChangeText = (payload) => setText(payload);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-      <TouchableOpacity>
-        <Text style={styles.btnText}>Work</Text>
-      </TouchableOpacity>
-      <TouchableHighlight underlayColor={'yellow'} activeOpacity={0.5} onPress={()=>console.log('press')}>
-        <Text style={styles.btnText}>Travel</Text>
-      </TouchableHighlight>
+        <TouchableOpacity onPress={work}>
+          <Text
+            style={{ ...styles.btnText, color: working ? 'white' : theme.grey }}
+          >
+            Work
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={travel}>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? 'white' : theme.grey,
+            }}
+          >
+            Travel
+          </Text>
+        </TouchableOpacity>
       </View>
+
+      <TextInput
+        onChangeText={onChangeText}
+        value={text}
+        returnKeyType="send"
+        placeholder={working ? 'Add A To Do' : 'Where do you want to go'}
+        style={styles.input}
+      ></TextInput>
     </View>
   );
 }
@@ -22,17 +58,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000',
-    paddingHorizontal:20,
+    paddingHorizontal: 20,
   },
   header: {
-    flexDirection:'row',
-    justifyContent:'space-between',
-    marginTop:100,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 100,
   },
   btnText: {
-    fontSize:30,
-    fontWeight:'700',
-    // color:theme.grey,
-    color:'white',
-  }
+    fontSize: 30,
+    fontWeight: '700',
+  },
+  input: {
+    backgroundColor: 'white',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
+  },
 });
